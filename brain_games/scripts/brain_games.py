17f942f0bf339167bcfaea5b74b5323a71369e4c
herovnik <nikonov.daniil.d@gmail.com>
2025-04-01 16:31:21 +0300

    Im tired of this, I dont know how much commits i have already made

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,9 +1,9 @@
 import sys
 sys.path.append("/home/nikon/test/project-1/python-project-49/brain_games/")
-import cli
+import brain_games.cli
 
 def main():
-    cli.welcome_user()
+    brain_games.cli.welcome_user()
     #return name
 
     

a679f96466358ee8fac0b51eb2d5454b686d8069
herovnik <nikonov.daniil.d@gmail.com>
2025-04-01 16:26:53 +0300

    Commit num that i dont know

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -3,8 +3,8 @@ sys.path.append("/home/nikon/test/project-1/python-project-49/brain_games/")
 import cli
 
 def main():
-    name = cli.welcome_user()
-    return name
+    cli.welcome_user()
+    #return name
 
     
 
